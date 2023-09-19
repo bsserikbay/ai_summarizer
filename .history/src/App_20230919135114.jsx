87b9c3ca8ca69ef76b472900772f2c90db6c00a5
Serikbayev, Serik (EPAM Systems) <serik.serikbayev@astrazeneca.com>
2023-09-19 14:00:24 +0600

    Create project

---
+++ .history/src/App_20230919135114.jsx
@@ -0,0 +1,12 @@
+import Hero from './components/Hero'
+import Demo from './components/Demo'
+
+const App = () => {
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Hero from './components/Hero'
+import Demo from './components/Demo'
+import './App.css';
+
+const App = () => {
+  return (
+      <main>
+          <div className='main'>
+              <div className='gradient'/> 
+                  
+              
+          </div>
+      
+    </main>
+  )
+}
+
+export default App
