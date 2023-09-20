d40df84b6f416ab37b6c8e1faa8a0f6708ca0d73
Serikbayev, Serik (EPAM Systems) <serik.serikbayev@astrazeneca.com>
2023-09-20 16:20:29 +0600

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	build: {
-		outDir: "build"
+export default defineConfig(({ command }) => {
+	const config = {
+		plugins: [react()],
+		base: "/"
+	};
+
+	if (command !== "serve") {
+		config.base = "/ai_summarizer/";
 	}
+
+	return config;
 });
 
 
